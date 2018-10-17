80b752851a61fb721c50f8cb3f6671721ce0fcd3
paivarodrigo <rodrigo.buepa@gmail.com>
2018-10-17 02:48:27 -0300

    updated

--- application.py
+++ application.py
@@ -1,41 +1,71 @@
-import json, requests
-import urllib3
+import math
+import requests
+import numpy as np
+import pickle
+import pandas as pd
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-#    response = requests.get("http://smartqueueapi.azurewebsites.net/api/produtos/listarprodutos")
-    
-    data = {"Email":"jefferson.teste@gmail.com", "Senha":"123456"}
-#    response = requests.post('http://smartqueueapi.azurewebsites.net/api/usuarios/logar', data = data)
-# =============================================================================
-#     req = u.Request('http://smartqueueapi.azurewebsites.net/api/usuarios/logar')
-#     req.add_header('Content-Type','application/json')
-#     response = u.urlopen(req, data)
-#     
-# =============================================================================
-    
-    encoded_body = json.dumps({ "Email": "jefferson.teste@gmail.com", "Senha": "123456" })
-    http = urllib3.PoolManager()
+@app.route('/train')
+def train():
+    response = requests.get('http://smartqueueapi.azurewebsites.net/api/estudos/buscardadostreinoia')
+    response_json = response.json()
+    columns = ['horaSolicitacao', 'horaCheckin', 'qtdPessoas', 'qtdEnt', 'qtdPrincipal', 'qtdBebidas', 'qtdSobremesas', 'fds', 'noite', 'minutosUtilizados']    
+    df = pd.DataFrame(response_json, columns = columns)
+    df_ordenada = df.sort_values(by='horaCheckin', ascending=False)
+    maior_valor = df_ordenada.values[0,1] #será utilizado para mostrar o valor da saida
+    df_tratada = df.div(maior_valor)
+    df_x = df_tratada.iloc[:,:-1]
+    df_y = df_tratada.iloc[:,9]
+    x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.1, random_state=4)
+    model = MLPRegressor(activation='tanh', alpha=1e-05, batch_size='auto', beta_1=0.9,
+                         beta_2=0.999, early_stopping=True, epsilon=1e-09,
+                         hidden_layer_sizes=(90, 40), learning_rate='constant',
+                         learning_rate_init=0.001, max_iter=200, momentum=0.9,
+                         nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
+                         solver='adam', tol=0.0001, validation_fraction=0.1, verbose=False,
+                         warm_start=False)
+    model.fit(x_train, y_train)
+    filename = 'modelo_finalizado.sav'
+    pickle.dump(model, open(filename, 'wb'))
+    accuracy = model.score(df_x, df_y)
+    return 'Acurácia: ' + str(accuracy)
 
-    response = http.request('POST', 'http://smartqueueapi.azurewebsites.net/api/usuarios/logar',
-                 headers={'Content-Type': 'application/json'},
-                 body=encoded_body)
-    return str(json.loads(response.data.decode('utf-8')))
+@app.route('/test')
+def test():
+    response = requests.get('http://smartqueueapi.azurewebsites.net/api/estudos/buscardadostesteia')
+    response_json = response.json()
+    columns = ['horaSolicitacao', 'horaCheckin', 'qtdPessoas', 'qtdEnt', 'qtdPrincipal', 'qtdBebidas', 'qtdSobremesas', 'fds', 'noite', 'minutosUtilizados']    
+    df = pd.DataFrame(response_json, columns = columns)
+    df_ordenada = df.sort_values(by='horaCheckin', ascending=False)
+    maior_valor = df_ordenada.values[0,1] #será utilizado para mostrar o valor da saida
+    df_tratada = df.div(maior_valor)
+    df_x = df_tratada.iloc[:,:-1]
+    df_y = df_tratada.iloc[:,9]
+    filename  ='modelo_finalizado.sav'
+    loaded_model = pickle.load(open(filename, 'rb'))
+    prediction = loaded_model.predict(df_x)
+    prediction = np.multiply(prediction, maior_valor)
+    prediction.sort()
+    final_result = np.average(prediction[:5])
+    accuracy = loaded_model.score(df_x, df_y)    
+    return 'Resultado Final: ' + str(final_result) + ' Acurácia: ' + str(accuracy)
 
-@app.route("/get")
-def gette():
-    http = urllib3.PoolManager()
-    r = http.request('GET', 'http://smartqueueapi.azurewebsites.net/api/estudos/buscardadosexecucao')
-    return str(json.loads(r.data.decode('utf-8')))
-@app.route("/geti")
-def getti():
-    http = urllib3.PoolManager()
-    r = http.request('GET', 'http://smartqueueapi.azurewebsites.net/api/estudos/buscardadostreinoia')
-    return str(json.loads(r.data.decode('utf-8')))
-@app.route("/geto")
-def getto():
-    http = urllib3.PoolManager()
-    r = http.request('GET', 'http://smartqueueapi.azurewebsites.net/api/estudos/buscardadostesteia')
-    return str(json.loads(r.data.decode('utf-8')))
+@app.route('/run')
+def run():
+    response = requests.get('http://smartqueueapi.azurewebsites.net/api/estudos/buscardadosexecucao')
+    response_json = response.json()
+    columns = ['horaSolicitacao', 'horaCheckin', 'qtdPessoas', 'qtdEnt', 'qtdPrincipal', 'qtdBebidas', 'qtdSobremesas', 'fds', 'noite']    
+    df = pd.DataFrame(response_json, columns = columns)
+    df_ordenada = df.sort_values(by='horaCheckin', ascending=False)
+    maior_valor = df_ordenada.values[0,1] #será utilizado para mostrar o valor da saida
+    df_tratada = df.div(maior_valor)
+    filename  ='modelo_finalizado.sav'
+    loaded_model = pickle.load(open(filename, 'rb'))    
+    prediction = loaded_model.predict(df_tratada)
+    prediction = np.multiply(prediction, maior_valor)
+    prediction.sort()
+    final_result = np.average(prediction[:5])
+    return str(math.ceil(final_result))
